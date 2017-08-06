23960dbdf53cd907fd32ec349f5569a131371a24
Przemysław Owsianik <Przemek.Owsianik@yahoo.com>
2017-08-06 19:13:12 +0200

    Add project files.

---
+++ IrisPlantClassification/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrisPlantClassification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
